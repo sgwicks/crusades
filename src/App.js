81b2a8b23e6710162fc58edf001c7097fa58e853
sgwicks <samgwicks@gmail.com>
2020-05-19 11:47:56 +0100

    Main screen now has basic layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import './App.css';
 import MainScreen from './components/MainScreen';
 
 function App() {
--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#002642'
-  },
-  text: {
-    color: '#e5dada'
-  }
-});
-
 const MainScreen = () => {
+  const [troops] = useState(1000);
+  const [wealth] = useState(100);
+  const [unrest] = useState(50);
+  const [loyalty] = useState(75);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Troops</Text>
-      <Text style={styles.text}>Wealth</Text>
-      <Text style={styles.text}>Castle</Text>
-      <Text style={styles.text}>Unrest</Text>
-      <Text style={styles.text}>Loyalty</Text>
+      <Text style={styles.troops}>{troops}</Text>
+      <Text style={styles.wealth}>{wealth}</Text>
+      <Text style={styles.castle}>Castle</Text>
+      <Text style={styles.unrest}>{unrest}</Text>
+      <Text style={styles.loyalty}>{loyalty}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#002642',
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    flexFlow: 'row wrap'
+  },
+  troops: {
+    backgroundColor: '#B54B19',
+    color: '#e5dada',
+    width: '50%'
+  },
+  wealth: {
+    backgroundColor: '#E59500',
+    color: '#02040F',
+    width: '50%'
+  },
+  unrest: {
+    backgroundColor: '#02040F',
+    color: '#e5dada',
+    width: '50%'
+  },
+  loyalty: {
+    backgroundColor: '#840032',
+    color: '#e5dada',
+    width: '50%'
+  },
+  castle: {
+    color: '#e5dada',
+    width: '100%',
+    textAlign: 'center'
+  }
+});
 
 export default MainScreen;
