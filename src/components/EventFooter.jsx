bff7848731c3b5305a88e0c449db2bc8eea8a35f
sgwicks <samgwicks@gmail.com>
2020-05-27 10:59:55 +0100

    Style event footer with rounded top border

--- src/components/EventFooter.jsx
+++ src/components/EventFooter.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 const EventFooter = ({ toggleModal, event_name }) => {
   return (
-    <TouchableOpacity
-      onPress={toggleModal}
-      style={{ alignItems: 'center', justifyContent: 'center', width: '100%' }}>
-      <Text style={{ color: '#e5dada' }}>A new event: {event_name}!</Text>
+    <TouchableOpacity onPress={toggleModal} style={styles.footer}>
+      <Text style={styles.eventTitle}>Event</Text>
+      <Text style={styles.eventText}>{event_name}</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  footer: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    marginHorizontal: '5%',
+    padding: '30px',
+    borderTopLeftRadius: '30px',
+    borderTopRightRadius: '30px',
+    borderColor: 'rgba(229, 184, 110, 1)',
+    borderWidth: 5,
+    borderBottomWidth: 0,
+    borderTopStyle: 'ridge',
+    borderLeftStyle: 'ridge',
+    borderRightStyle: 'groove',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+  eventText: { color: '#e5dada' },
+  eventTitle: { color: '#e5dada' }
+});
 
 export default EventFooter;
